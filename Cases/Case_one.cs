8e156846aff1e7d52aba09dec790ad36a64c79ec
bilguun <bilguunbek1108@gmail.com>
2022-03-27 18:57:19 +0800

    Added C# Sample

---
+++ Cases/Case_one.cs
@@ -0,0 +1,11 @@
+using System;
+
+//Sample Program To Test Packer
+namespace HelloWorldApp {
+    class Case_One {
+        static void Main(string[] args) {
+            Console.WriteLine("Pack Me");
+            Console.ReadKey();
+        }
+    }
+}
