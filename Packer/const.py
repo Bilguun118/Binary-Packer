b1830049a6ccac3d295bf35a07746b9dc78ea9c8
bilguun <bilguunbek1108@gmail.com>
2022-05-01 21:14:56 +0800

    Added Bin

--- Packer/const.py
+++ Packer/const.py
@@ -1,4 +1,7 @@
 # тогтмол string утга
+# Программын нэр
 APPLICATIONNAME = ".NET PE Binary Packer"
+# Binary файл унших
 READBIN = "r+b"
+# Binary файлруу бичих
 WRITEBIN = "wb"
--- Packer/pack.py
+++ Packer/pack.py
@@ -1,7 +1,9 @@
+from lief import PE
 import const
 import lief
 import time
-
+import pefile
+import pydasm
 # Binary Pack хийх класс
 class BinPack(object):
     
@@ -20,17 +22,57 @@ class BinPack(object):
 
     # Command Line-д өгөгдсөн утгийг хүлээж аван python кодод унших функц
     def open_bin_dat(self):
-        data = open(self.args.input, const.READBIN)
-        bytesdata = data.read()
-        return bytesdata
+        pe = pefile.PE(self.args.input)
+        for section in pe.sections:
+            print(section.Name, hex(section.VirtualAddress),
+            hex(section.Misc_VirtualSize), section.SizeOfRawData )
+        pe.parse_data_directories()
+
+        for entry in pe.DIRECTORY_ENTRY_IMPORT:
+            print(entry.dll)
+            for imp in entry.imports:
+                print('\t', hex(imp.address), imp.name)
+        ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+        ep_ava = ep+pe.OPTIONAL_HEADER.ImageBase
+        data = pe.get_memory_mapped_image()[ep:ep+100]
+        offset = 0
+        while offset < len(data):
+          i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
+          print(pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset))
+          offset += i.length
     
-    # def unpack_bin_dat(self):
-    #     pack_data = self.open_bin_dat(self)
-    #     unpack_PE = lief.PE.parse(self.args.p)
-    #     file_alignment = unpack_PE.optional_header.file_alignment
-    #     section_alignment = unpack_PE.optional_header.section_alignment
-    #     packed_data = list(pack_data)
-    #     packed_data = self.pad_data()
+    def build_bin_dat(self, bin_data, section):
+        packed_binary = PE.Binary("C:/Users/Bilguun/Desktop/NewPacked.exe", PE.PE_TYPE.NET)
+        section_text = PE.Section(".text")
+        data = list(map(ord, section))
+        section_text.content = data
+        section_text.virtual_address = 0x1000
+        builder = lief.PE.Builder(packed_binary)
+        builder.build_imports(True)
+        builder.build()
+        builder.write("NewPacked.exe")
+
+
+    def read_bin_data(self):
+        binary_data = lief.PE.parse(self.args.input)
+        self.printdetails(binary_data)
+        text_section = binary_data.get_section(".text")
+        return binary_data, bytes(text_section.content)
+
+    def printdetails(self, binary_data):
+        print("Программын DOS HEADER", "-"*100)
+        print(binary_data.dos_header)
+        print("Программын HEADER", "-"*100)
+        print(binary_data.header)
+        print("Программын OPTIONAL HEADER", "-"*100)
+        print(binary_data.optional_header)
+        print("Программын EntryPoint хаяг: ", hex(binary_data.optional_header.addressof_entrypoint))
+        print("Программын section бүрэлдэхүүн хэсгүүд:" , "-"*100)
+        for section in binary_data.sections:
+            print(section)
+        for imported_library in binary_data.imports:
+          print("Import хийсэн сан: " + imported_library.name)
+
 
 # Программын нэр болон тайлбарыг эхлэлд хэвлэх 
 def PrintBeginning():
